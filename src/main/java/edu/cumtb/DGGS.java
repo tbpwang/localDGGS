f2bbc12f61cb37a0af35abcd03bc04b0269d5894
tbpwang <tbpwang@gmail.com>
2016-09-18 15:46:48 +0800

    Initial commit

---
+++ src/main/java/edu/cumtb/DGGS.java
@@ -0,0 +1,16 @@
+package edu.cumtb;
+
+import gov.nasa.worldwind.render.DrawContext;
+import gov.nasa.worldwind.render.Renderable;
+import gov.nasa.worldwindx.examples.ApplicationTemplate;
+
+/**
+ * Administrator
+ * Created by tbpwang
+ * 2016/8/26.
+ */
+public class DGGS extends ApplicationTemplate implements Renderable {
+    public void render(DrawContext drawContext) {
+
+    }
+}
---
+++ src/main/java/edu/cumtb/model/QTM.java
@@ -0,0 +1,16 @@
+package edu.cumtb.model;
+
+import edu.cumtb.DGGS;
+import gov.nasa.worldwind.render.DrawContext;
+
+/**
+ * Administrator
+ * Created by tbpwang
+ * 2016/9/18.
+ */
+public class QTM extends DGGS {
+    @Override
+    public void render(DrawContext drawContext) {
+        super.render(drawContext);
+    }
+}
